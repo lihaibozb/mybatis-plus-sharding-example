7d2eef951b9271c9ceee21ddfc0825c83bca85ec
lihaibozb <lihaibo19861126@126.com>
2019-07-11 10:12:22 +0800

    first commit

---
+++ src/main/java/com/zyxfcfc/mybatisplussharding/MybatisPlusShardingApplication.java
@@ -0,0 +1,13 @@
+package com.zyxfcfc.mybatisplussharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusShardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisPlusShardingApplication.class, args);
+	}
+
+}
